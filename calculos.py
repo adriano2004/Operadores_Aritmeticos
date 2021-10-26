f09cfe88deaccb3a17424047d0f04d04d0fffa47
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-10-25 21:05:27 -0300

    Operadores

---
+++ calculos.py
@@ -0,0 +1,11 @@
+n1=int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+so= n1+n2
+su = n1 - n2
+m= n1*n2
+d= n1/n2
+di= n1//n2
+e=n1**n2
+print(' A soma é {} \n A subtração é {} \n A multiplicação é {} \n A divisão é {:.2f} \n A divisão inteira é {} \n A potência é {}'.format(so,su,m,d,di,e))
+
